17e75f5cd81057ef98a9679bb25b6d89750d8d8a
Alexandre Philibeaux <aphilibeaux@scaleway.com>
2022-02-28 10:21:47 +0000

    feat(teardown): add delete container, dns

--- dns.go
+++ dns.go
@@ -2,27 +2,107 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	container "github.com/scaleway/scaleway-sdk-go/api/container/v1beta1"
 	domain "github.com/scaleway/scaleway-sdk-go/api/domain/v2beta1"
 	"github.com/scaleway/scaleway-sdk-go/scw"
 )
 
-// func ListDNS(
-// 	client *scw.Client,
-// 	DNSName string,
-// ) {
-// 	fmt.Println("waiting for dns to be ready")
+const (
+	waitForDNSDefaultTimeout = 15 * time.Minute
+	defaultRetryInterval     = 5 * time.Second
+)
 
-// 	api := domain.NewAPI(client)
+const (
+	TTL  = uint32(360)
+	TYPE = "CNAME"
+)
 
-// 	ZonesDNS, err := api.ListDNSZones(&domain.ListDNSZonesRequest{
-// 		DNSZone: DNSName,
-// 	})
+func WaitForDNS(
+	client *scw.Client,
+	DNSZone string,
+) (*domain.DNSZone, error) {
+	timeout := waitForDNSDefaultTimeout
+	retryInterval := defaultRetryInterval
 
-// }
+	api := domain.NewAPI(client)
 
-func SetDNSRecord(
+	terminalStatus := map[domain.DNSZoneStatus]struct{}{
+		domain.DNSZoneStatusActive: {},
+		domain.DNSZoneStatusLocked: {},
+		domain.DNSZoneStatusError:  {},
+	}
+
+	dns, err := WaitSync(&WaitSyncConfig{
+		Get: func() (interface{}, bool, error) {
+			// listing dns zones and take the first one
+			DNSZones, err := api.ListDNSZones(&domain.ListDNSZonesRequest{
+				DNSZone: DNSZone,
+			})
+
+			if err != nil {
+				return nil, false, err
+			}
+
+			Dns := DNSZones.DNSZones[0]
+
+			_, isTerminal := terminalStatus[Dns.Status]
+
+			return Dns, isTerminal, nil
+		},
+		Timeout:          timeout,
+		IntervalStrategy: LinearIntervalStrategy(retryInterval),
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("error waiting for DNS zone to be ready: %s", err)
+	}
+
+	return dns.(*domain.DNSZone), nil
+}
+
+func DeleteDNSRecord(
+	client *scw.Client,
+	DNSZone string,
+	Container *container.Container,
+) (*domain.UpdateDNSZoneRecordsResponse, error) {
+	fmt.Println("Update Zone DNS")
+
+	api := domain.NewAPI(client)
+
+	Data := Container.DomainName + "."
+
+	IDFields := &domain.RecordIdentifier{
+		Name: Container.Name,
+		Data: &Data,
+		Type: TYPE,
+		//TTL:  TTL,
+	}
+
+	Changes := []*domain.RecordChange{
+		{
+			Delete: &domain.RecordChangeDelete{
+				IDFields: IDFields,
+			},
+		},
+	}
+
+	records, err := api.UpdateDNSZoneRecords(
+		&domain.UpdateDNSZoneRecordsRequest{
+			DNSZone: DNSZone,
+			Changes: Changes,
+		})
+
+	if err != nil {
+
+		return nil, err
+	}
+
+	return records, nil
+}
+
+func AddDNSRecord(
 	client *scw.Client,
 	DNSZone string,
 	Container *container.Container,
@@ -38,8 +118,8 @@ func SetDNSRecord(
 	Records := []*domain.Record{
 		{
 			Name: Container.Name,
-			Type: "CNAME",
-			TTL:  60,
+			Type: TYPE,
+			TTL:  TTL,
 			Data: Data,
 		},
 	}
@@ -64,5 +144,4 @@ func SetDNSRecord(
 	}
 
 	return records, nil
-
 }
